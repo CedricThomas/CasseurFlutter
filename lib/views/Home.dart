6aa270a51025caadeb2b332275b4c37c6d1f4cd6
Cédric Thomas <cedric@reyah.eu>
2020-12-30 13:57:06 +0100

    [+] add analyser

--- lib/views/Home.dart
+++ lib/views/Home.dart
@@ -1,11 +1,6 @@
-import 'dart:convert';
-import 'package:flutter/cupertino.dart';
+import 'package:casseurflutter/services/api.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:http/http.dart' as http;
-import 'package:flutter_appauth/flutter_appauth.dart';
 
-import '../constants.dart';
 import '../widgets/login.dart';
 import '../widgets/profile.dart';
 
@@ -22,8 +17,8 @@ class _HomeState extends State<Home> {
   String errorMessage;
   String name;
   String picture;
-  final FlutterAppAuth appAuth = FlutterAppAuth();
-  final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
+
+  APIService api = APIService();
 
   @override
   Widget build(BuildContext context) {
@@ -31,126 +26,26 @@ class _HomeState extends State<Home> {
       title: 'Auth0 Demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Auth0 Demo'),
+          title: Text('Login'),
         ),
         body: Center(
           child: isBusy
               ? CircularProgressIndicator()
-              : isLoggedIn
-                  ? Profile(logoutAction, name, picture)
-                  : Login(loginAction, errorMessage),
+              : api.isLoggedIn
+                  ? Profile(api.logout, name, picture)
+                  : Login(api.login, errorMessage),
         ),
       ),
     );
   }
 
-  Map<String, dynamic> parseIdToken(String idToken) {
-    final parts = idToken.split(r'.');
-    assert(parts.length == 3);
-
-    return jsonDecode(
-        utf8.decode(base64Url.decode(base64Url.normalize(parts[1]))));
-  }
-
-  Future<Map<String, dynamic>> getUserDetails(String accessToken) async {
-    final url = 'https://$AUTH0_DOMAIN/userinfo';
-    final response = await http.get(
-      url,
-      headers: {'Authorization': 'Bearer $accessToken'},
-    );
-
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
-    } else {
-      throw Exception('Failed to get user details');
-    }
-  }
-
-  Future<void> loginAction() async {
-    setState(() {
-      isBusy = true;
-      errorMessage = '';
-    });
-
-    try {
-      final AuthorizationTokenResponse result =
-          await appAuth.authorizeAndExchangeCode(
-        AuthorizationTokenRequest(
-          AUTH0_CLIENT_ID,
-          AUTH0_REDIRECT_URI,
-          issuer: 'https://$AUTH0_DOMAIN',
-          scopes: ['openid', 'profile', 'offline_access'],
-          // promptValues: ['login']
-        ),
-      );
-
-      final idToken = parseIdToken(result.idToken);
-      final profile = await getUserDetails(result.accessToken);
-
-      await secureStorage.write(
-          key: 'refresh_token', value: result.refreshToken);
-
-      setState(() {
-        isBusy = false;
-        isLoggedIn = true;
-        name = idToken['name'];
-        picture = profile['picture'];
-      });
-    } catch (e, s) {
-      print('login error: $e - stack: $s');
-
-      setState(() {
-        isBusy = false;
-        isLoggedIn = false;
-        errorMessage = e.toString();
-      });
-    }
-  }
-
-  void logoutAction() async {
-    await secureStorage.delete(key: 'refresh_token');
-    setState(() {
-      isLoggedIn = false;
-      isBusy = false;
-    });
-  }
-
   @override
   void initState() {
-    initAction();
-    super.initState();
-  }
-
-  void initAction() async {
-    final storedRefreshToken = await secureStorage.read(key: 'refresh_token');
-    if (storedRefreshToken == null) return;
-
-    setState(() {
-      isBusy = true;
-    });
-
     try {
-      final response = await appAuth.token(TokenRequest(
-        AUTH0_CLIENT_ID,
-        AUTH0_REDIRECT_URI,
-        issuer: AUTH0_ISSUER,
-        refreshToken: storedRefreshToken,
-      ));
-
-      final idToken = parseIdToken(response.idToken);
-      final profile = await getUserDetails(response.accessToken);
-
-      secureStorage.write(key: 'refresh_token', value: response.refreshToken);
-
-      setState(() {
-        isBusy = false;
-        isLoggedIn = true;
-        name = idToken['name'];
-        picture = profile['picture'];
-      });
-    } catch (e, s) {
-      print('error on refresh token: $e - stack: $s');
-      logoutAction();
+      api.refreshToken();
+    } on RefreshException catch (e) {
+      errorMessage = e.toString();
     }
+    super.initState();
   }
 }
