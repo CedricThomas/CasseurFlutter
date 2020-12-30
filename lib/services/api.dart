875952e81ee1616bcfb53b73a38c057192accdf9
Cédric Thomas <cedric@reyah.eu>
2020-12-30 14:06:36 +0100

    [+] add analyse

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -7,44 +7,42 @@ import 'package:flutter_appauth/flutter_appauth.dart';
 import '../constants.dart';
 
 class NotLoggedException implements Exception {
-  final String msg;
   const NotLoggedException(this.msg);
+  final String msg;
+  @override
   String toString() => 'NotLoggedException: $msg';
 }
 
 class RefreshException implements Exception {
-  final String msg;
   const RefreshException(this.msg);
+  final String msg;
+  @override
   String toString() => 'RefreshException: $msg';
 }
 
 class APIService {
-  final FlutterAppAuth appAuth;
-  final FlutterSecureStorage secureStorage;
-  bool _isLoggedIn;
-  String _accessToken;
-
-  bool get isLoggedIn => _isLoggedIn;
+  factory APIService() => _instance;
 
   APIService._privateConstructor()
       : appAuth = FlutterAppAuth(),
         secureStorage = const FlutterSecureStorage(),
-        _isLoggedIn = false,
-        _accessToken = null;
+        _isLoggedIn = false;
+
+  final FlutterAppAuth appAuth;
+  final FlutterSecureStorage secureStorage;
+  bool _isLoggedIn;
+
+  bool get isLoggedIn => _isLoggedIn;
 
   static final APIService _instance = APIService._privateConstructor();
 
-  factory APIService() {
-    return _instance;
-  }
-
   Future<UserInfo> getUserInfo(String _accessToken) async {
     if (!isLoggedIn) {
-      throw NotLoggedException('Not logged in');
+      throw const NotLoggedException('Not logged in');
     }
 
-    final url = 'https://$AUTH0_DOMAIN/userinfo';
-    final response = await http.get(
+    final String url = 'https://$AUTH0_DOMAIN/userinfo';
+    final http.Response response = await http.get(
       url,
       headers: {'Authorization': 'Bearer $_accessToken'},
     );
@@ -70,18 +68,19 @@ class APIService {
     _isLoggedIn = true;
   }
 
-  void logout() async {
+  Future<void> logout() async {
     await secureStorage.delete(key: 'refresh_token');
     _isLoggedIn = false;
   }
 
-  void refreshToken() async {
+  Future<void> refreshToken() async {
     _isLoggedIn = false;
-    final storedRefreshToken = await secureStorage.read(key: 'refresh_token');
+    final String storedRefreshToken =
+        await secureStorage.read(key: 'refresh_token');
     if (storedRefreshToken == null) {
-      throw RefreshException('Not refresh token found');
+      throw const RefreshException('Not refresh token found');
     }
-    final response = await appAuth.token(TokenRequest(
+    final TokenResponse response = await appAuth.token(TokenRequest(
       AUTH0_CLIENT_ID,
       AUTH0_REDIRECT_URI,
       issuer: AUTH0_ISSUER,
@@ -89,7 +88,6 @@ class APIService {
     ));
 
     secureStorage.write(key: 'refresh_token', value: response.refreshToken);
-    _accessToken = response.accessToken;
     _isLoggedIn = true;
   }
 }
