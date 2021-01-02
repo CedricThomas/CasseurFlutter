c2deb8792cb8667069d47e75ebbd178e47d79d5b
Arthur Knoepflin <arthur@knoepflin.eu>
2021-01-02 17:34:10 +0100

    [+] geolocation

--- lib/views/CreateMemo.dart
+++ lib/views/CreateMemo.dart
@@ -3,6 +3,8 @@ import 'package:casseurflutter/models/memo.dart';
 import 'package:casseurflutter/services/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:progress_state_button/progress_button.dart';
 
 class CreateMemo extends StatefulWidget {
   static const String id = '/createMemo';
@@ -15,7 +17,56 @@ class _CreateMemo extends State<CreateMemo> {
   bool _isLoading = false;
   String _memoTitle = '';
   String _memoContent = '';
+  Position _memoPosition;
+  ButtonState _locationButtonState = ButtonState.idle;
   CreateMemoBloc _memoBloc;
+
+  Future<void> _onGeolocationAsked() async {
+    if (_locationButtonState == ButtonState.idle) {
+      try {
+        setState(() {
+          _locationButtonState = ButtonState.loading;
+        });
+        final LocationPermission perm = await Geolocator.checkPermission();
+        if (perm != LocationPermission.always &&
+            perm != LocationPermission.whileInUse) {
+          final LocationPermission newPerm =
+              await Geolocator.requestPermission();
+          if (newPerm != LocationPermission.always &&
+              newPerm != LocationPermission.whileInUse) {
+            setState(() {
+              _locationButtonState = ButtonState.fail;
+              _memoPosition = null;
+            });
+            return;
+          }
+        }
+        final Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high,
+          timeLimit: const Duration(seconds: 20),
+        );
+        setState(() {
+          _memoPosition = position;
+          _locationButtonState = ButtonState.success;
+        });
+      } catch (e) {
+        setState(() {
+          _locationButtonState = ButtonState.fail;
+          _memoPosition = null;
+        });
+      }
+    } else if (_locationButtonState == ButtonState.success) {
+      setState(() {
+        _locationButtonState = ButtonState.idle;
+        _memoPosition = null;
+      });
+    } else if (_locationButtonState == ButtonState.fail) {
+      setState(() {
+        _locationButtonState = ButtonState.idle;
+        _memoPosition = null;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +86,13 @@ class _CreateMemo extends State<CreateMemo> {
               ? null
               : () {
                   _memoBloc.add(StartCreateMemo(
-                      memo: CreateMemoRequest(_memoTitle, _memoContent, null)));
+                      memo: CreateMemoRequest(
+                          _memoTitle,
+                          _memoContent,
+                          (_memoPosition != null)
+                              ? Location(_memoPosition.latitude,
+                                  _memoPosition.longitude)
+                              : null)));
                 },
           child: _isLoading
               ? const CircularProgressIndicator(backgroundColor: Colors.white)
@@ -113,6 +170,52 @@ class _CreateMemo extends State<CreateMemo> {
                           ),
                         ),
                       ),
+                      Container(
+                        height: 30,
+                      ),
+                      ProgressButton(
+                        stateWidgets: const <ButtonState, Widget>{
+                          ButtonState.idle: Text(
+                            'Add a geolocation',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          ButtonState.loading: Text(
+                            'Locating',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          ButtonState.fail: Text(
+                            'Could not get position',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          ButtonState.success: Text(
+                            'Geolocation added',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500),
+                          )
+                        },
+                        stateColors: <ButtonState, Color>{
+                          ButtonState.idle: Colors.blue,
+                          ButtonState.loading: Colors.blue.shade300,
+                          ButtonState.fail: Colors.red.shade300,
+                          ButtonState.success: Colors.green.shade400,
+                        },
+                        radius: 5.0,
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 10),
+                        onPressed: _onGeolocationAsked,
+                        state: _locationButtonState,
+                      ),
+                      // ElevatedButton(
+                      //   onPressed: () {},
+                      //   child: const Text('Add a geolocation'),
+                      // ),
                     ],
                   ),
                 ),
